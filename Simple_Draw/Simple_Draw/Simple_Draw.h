0868b43b7f7b4bcd28bec1c75c8ad4ac9ab24ae6
unknown <Damme88@Damme88-ToanPT.(none)>
2012-06-24 15:15:27 +0700

    Added Simple_Draw

---
+++ Simple_Draw/Simple_Draw/Simple_Draw.h
@@ -0,0 +1,38 @@
+
+// Simple_Draw.h : main header file for the Simple_Draw application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CSimple_DrawApp:
+// See Simple_Draw.cpp for the implementation of this class
+//
+
+class CSimple_DrawApp : public CWinAppEx
+{
+public:
+	CSimple_DrawApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSimple_DrawApp theApp;
